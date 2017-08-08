23ecaa57d2bd2c99dcc0dab8890246b5b7303b4c
Jeremiah Ogutu <jeremiahogutu@protonmail.com>
2017-08-08 12:33:42 -0400

    added setinterval method

--- js/script.js
+++ js/script.js
@@ -3,21 +3,25 @@ const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
 
+function runClock(){
+	var date = new Date();
+	console.log(date);
 
-var date = new Date();
-console.log(date);
-
-let hr = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
+	let hr = date.getHours();
+	let min = date.getMinutes();
+	let sec = date.getSeconds();
 
 
-let hrPosition = (hr*360/12)+(min*(360/60)/12);
-let minPosition = (min*360/60)+(sec*(360/60)/60);
-let secPosition = sec*360/60;
+	let hrPosition = (hr*360/12)+(min*(360/60)/12);
+	let minPosition = (min*360/60)+(sec*(360/60)/60);
+	let secPosition = sec*360/60;
 
-console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
+	console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
 
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+	HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+	MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+	SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+
+}
+
+var interval = setInterval(runClock, 1000);
