0fe0b37ccfe4cadb676e5b9283650fb5842c93ab
Jeremiah Ogutu <jeremiahogutu@protonmail.com>
2017-08-08 12:12:11 -0400

    fixed minute and secondhand positions

--- js/script.js
+++ js/script.js
@@ -14,6 +14,5 @@ let sec = date.getSeconds();
 console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
 
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + hrPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + hrPosition + "deg)";
-
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
